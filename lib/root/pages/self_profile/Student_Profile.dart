5ef80af497fe15e13a922c072668580aefb63e2c
Aditya Thakur <adityachauhan9456923436@gmail.com>
2023-03-07 14:32:12 +0530

    Profile of Student

--- lib/root/pages/self_profile/Student_Profile.dart
+++ lib/root/pages/self_profile/Student_Profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/Material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Student_Profile extends StatefulWidget {
   const Student_Profile({super.key});
@@ -9,6 +11,51 @@ class Student_Profile extends StatefulWidget {
 }
 
 class _Student_ProfileState extends State<Student_Profile> {
+  final List<Map<String, dynamic>> gridMap = [
+    {
+      "title": "white sneaker with adidas logo",
+      "price": "255",
+      "images":
+          "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80",
+    },
+    {
+      "title": "Black Jeans with blue stripes",
+      "price": "245",
+      "images":
+          "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Red shoes with black stripes",
+      "price": "155",
+      "images":
+          "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      "title": "Gamma shoes with beta brand.",
+      "price": "275",
+      "images":
+          "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Alpha t-shirt for alpha testers.",
+      "price": "25",
+      "images":
+          "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Beta jeans for beta testers",
+      "price": "27",
+      "images":
+          "https://images.unsplash.com/photo-1602293589930-45aad59ba3ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "V&V  model white t shirts.",
+      "price": "55",
+      "images":
+          "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    }
+  ];
+
   // // Text Editing Forms For Validation
   // TextEditingController uid = TextEditingController();
   // TextEditingController name = TextEditingController();
@@ -44,23 +91,74 @@ class _Student_ProfileState extends State<Student_Profile> {
     return Container(
         child: Column(
       children: [
-        TopCompelete(),
-        NameAndBio(),
-        Social(),
-        Divider(
-          indent: 10,
-          endIndent: 20,
-          color: Colors.grey.shade300,
-          thickness: 1,
-        ),
-        Logos(),
-        Divider(
-          indent: 10,
-          endIndent: 20,
-          color: Colors.grey.shade300,
-          thickness: 1,
-        ),
+        Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 2.0, color: Colors.grey.shade300),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green.shade300,
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ), //BoxShadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    spreadRadius: 0.0,
+                  ), //BoxShadow
+                ],
+                color: Colors.green.shade100,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15))),
+            child: Column(
+              children: [
+                TopCompelete(),
+                NameAndBio(),
+                Social(),
+                Logos(),
+              ],
+            )),
         Post(),
+        Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 2.0, color: Colors.green.shade300),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green.shade300,
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    spreadRadius: 0.0,
+                  ),
+                ],
+                shape: BoxShape.rectangle,
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.green.shade300,
+                    Colors.red,
+                    Colors.indigo.shade300
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15))),
+            child: PostContainer()),
       ],
     ));
   }
@@ -139,7 +237,7 @@ class _Student_ProfileState extends State<Student_Profile> {
                       shape: MaterialStateProperty.all(
                           const RoundedRectangleBorder(
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(10)))),
+                                  BorderRadius.all(Radius.circular(3)))),
                       padding: MaterialStateProperty.all(
                           const EdgeInsets.only(left: 25, right: 25)),
                       elevation: MaterialStateProperty.all(1),
@@ -160,7 +258,7 @@ class _Student_ProfileState extends State<Student_Profile> {
               "Student At GLA University ",
               style: TextStyle(
                 fontSize: 17,
-                color: Colors.grey.shade300,
+                color: Colors.grey.shade500,
               ),
 
               // style:
@@ -198,11 +296,11 @@ class _Student_ProfileState extends State<Student_Profile> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Text('$value',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
             SizedBox(height: 2),
             Text(
               Font,
-              style: TextStyle(fontSize: 15),
+              style: TextStyle(fontSize: 12),
             )
           ],
         ),
@@ -210,7 +308,7 @@ class _Student_ProfileState extends State<Student_Profile> {
 
   Widget Logos() {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
+      padding: const EdgeInsets.only(left: 40, right: 40, top: 10, bottom: 5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -252,29 +350,134 @@ class _Student_ProfileState extends State<Student_Profile> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  "POST",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      "POST",
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      speed: const Duration(milliseconds: 400),
+                    )
+                  ],
                 ),
-                Text(
-                  "SAVED",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic),
-                )
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      "SAVED",
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      speed: const Duration(milliseconds: 400),
+                    )
+                  ],
+                ),
               ],
             ),
-            Divider(
-              thickness: 1,
-              color: Colors.grey.shade300,
-            )
           ],
         ),
       ),
     );
+  }
+
+  Widget PostContainer() {
+    return GridView.builder(
+        padding: EdgeInsets.all(10),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          mainAxisExtent: 310,
+        ),
+        itemCount: gridMap.length,
+        itemBuilder: (_, index) {
+          return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  16.0,
+                ),
+                color: Colors.green.shade100,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0),
+                    ),
+                    child: Image.network(
+                      "${gridMap.elementAt(index)['images']}",
+                      height: 170,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${gridMap.elementAt(index)['title']}",
+                          style: Theme.of(context).textTheme.subtitle1!.merge(
+                                const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                        ),
+                        const SizedBox(
+                          height: 3.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.heart,
+                              size: 17,
+                              color: Colors.grey.shade500,
+                            ),
+                            Text(
+                              "${gridMap.elementAt(index)['price']}",
+                              style:
+                                  Theme.of(context).textTheme.subtitle2!.merge(
+                                        TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.grey.shade500,
+                                        ),
+                                      ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3.0,
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                CupertinoIcons.heart,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.comment_bank_outlined,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ));
+        });
   }
 }
