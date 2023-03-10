b55e6d99123e6f778473b58b59f651f95c55ab7c
ankit <you@example.com>
2023-03-11 00:52:41 +0530

    Splash Activity Added
    Splash Activity Add

---
+++ lib/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gla_engage/main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const MainPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.topRight,
+          colors: [
+            Color(0xFFFFFFFF),
+            Color(0xFFFFFFFF),
+          ],
+        )),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/icon/gla_light.png',
+              height: 500.0,
+              width: 300.0,
+              alignment: Alignment.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
